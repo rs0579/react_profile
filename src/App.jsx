a669bb03d97e0a68c7c1a02ac4960aca56981b9a
rs0579 <raheemsenegal@outlook.com>
2025-02-08 23:13:38 +0100

    moved nav to components folder and imported it to App.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Nav from './components/Nav'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,7 +11,7 @@ function App() {
     <>
       <body>
         <header> 
-          
+          <Nav />
         </header>
 
         <main> </main>
