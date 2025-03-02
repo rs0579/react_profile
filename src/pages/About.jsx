c09ed27c3d3b008b37c6808f418324cb3794b59a
rs0579 <raheemsenegal@outlook.com>
2025-03-02 21:20:12 +0100

    changed styles to the About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -12,7 +12,7 @@ export default function About() {
                 alt="Castellfollit de la Roca" />
 
             <section className="about-section">
-                <p className="about-text"> Parce que vous êtes un grand seigneur, vous vous croyez un grand génie !… noblesse, fortune, un rang, des places, tout cela rend si fier ! Qu’avez-vous fait pour tant de biens ? vous vous êtes donné la peine de naître, et rien de plus : du reste, homme assez ordinaire ! tandis que moi, morbleu, perdu dans la foule obscure, il m’a fallu déployer plus de science et de calculs pour subsister seulement, qu’on n’en a mis depuis cent ans à gouverner toutes les Espagnes ; et vous voulez jouter ! -Beaumarchais</p>
+                <p className="about-text"> Parce que vous êtes un grand seigneur, vous vous croyez un grand génie !… noblesse, fortune, un rang, des places, tout cela rend si fier ! Qu’avez-vous fait pour tant de biens ? vous vous êtes donné la peine de naître, et rien de plus : du reste, homme assez ordinaire ! tandis que moi, morbleu, perdu dans la foule obscure, il m’a fallu déployer plus de science et de calculs pour subsister seulement, qu’on n’en a mis depuis cent ans à gouverner toutes les Espagnes ; et vous voulez jouter ! <span>-Pierre-Augustin Caron de Beaumarchais</span></p>
             </section>
             <section className="about-section">
                 <p className="about-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum voluptas corporis impedit optio accusamus doloremque quod repellat deleniti labore, incidunt officiis dicta natus aut tenetur, aliquam dolorem adipisci assumenda nisi.
