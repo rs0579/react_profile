398a78f83097e74bd5a9902b215006397ce280fd
rs0579 <raheemsenegal@outlook.com>
2025-04-06 00:53:29 +0200

    changed the contact back to the newer code.

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,110 +1,110 @@
-// import { Button } from '@material-tailwind/react'
-
-// const Contact = () => {
-//     return (
-//         <main className="contact-background">
-//             <section className='text-center' id='contact-container'>
-//                 <h2 className="text-indigo-500" id="contact-h2">Why Not Team Up for an Awesome Project?</h2>
-//                 <h1 className="text-rose-500" id="contact-h1">Feel free to contact me!</h1>
-//                 <a href="mailto:thoth1718@gmail.com">
-//                     <Button id="contact-button" ripple={true}>Email Me</Button></a>
-//                 <p className="w-80 ...  h-80 ... "id="contact-text">Whether it is in the United States or internationally, I am eager to contribute my passion, dedication, and drive for growth as a developer to your projects!</p>
-//             </section>
-//         </main>
-//     )
-// }
-// export default Contact
-import { useState } from 'react'
 import { Button } from '@material-tailwind/react'
 
 const Contact = () => {
-    const [name, setName] = useState('')
-    const [email, setEmail] = useState('')
-    const [message, setMessage] = useState('')
-    const [errorMessage, setErrorMessage] = useState('')
-
-    const handleInputChange = (e) => {
-        const { target } = e;
-        const inputType = target.name
-        const inputValue = target.value
-
-        if (inputType === 'email') {
-            setEmail(inputValue)
-        } else if (inputType === 'name') {
-            setName(inputValue) ``
-        } else {
-            setMessage(inputValue)
-        }
-    };
-
-    const handleFormSubmit = (e) => {
-        e.preventDefault();
-
-        if (!name) {
-            setErrorMessage('Name data required')
-            return
-        }
-
-        if (!email) {
-            setErrorMessage('Email address required')
-            return
-        }
-
-        if (!message) {
-            setErrorMessage('Message data required')
-            return
-        }
-
-        // Construct mailto link
-        const mailtoLink = `raheemsenegal@outlook.com?subject=Contact from ${encodeURIComponent(name)}&body=${encodeURIComponent(message + "\n\nReply to: " + email)}`;
-        // Open default email client
-        window.location.href = mailtoLink;
-
-        // Reset the form fields
-        setName('')
-        setEmail('')
-        setMessage('')
-        setErrorMessage('') // Clear error message on successful submission
-    };
-
     return (
-        <section className='contact-background'>
-            <h2 className='email-me-h2'>Email Me 😊</h2>
-            <form className="form" onSubmit={handleFormSubmit}>
-                <input
-                    className='contact-input' id='contact-name'
-                    type="text"
-                    value={name}
-                    name='name'
-                    onChange={handleInputChange}
-                    placeholder='Name'
-                />
-                <input
-                    className='contact-input'
-                    id='email'
-                    type="email"
-                    value={email}
-                    name='email'
-                    onChange={handleInputChange}
-                    placeholder='Email'
-                />
-                <textarea
-                    id='textarea'
-                    value={message}
-                    name='message'
-                    onChange={handleInputChange}
-                    placeholder='Enter text here'
-                />
-                {/* <button type="submit">Submit</button> */}
-                <Button variant="outlined" type="submit" className="rounded-full">Submit</Button>
-            </form>
-            {errorMessage && (
-                <div>
-                    <p className="error-text">{errorMessage}</p>
-                </div>
-            )}
-        </section>
-    );
+        <main className="contact-background">
+            <section className='text-center' id='contact-container'>
+                <h2 className="text-indigo-500" id="contact-h2">Why Not Team Up for an Awesome Project?</h2>
+                <h1 className="text-rose-500" id="contact-h1">Feel free to contact me!</h1>
+                <a href="mailto:thoth1718@gmail.com">
+                    <Button id="contact-button" ripple={true}>Email Me</Button></a>
+                <p className="w-80 ...  h-80 ... "id="contact-text">Whether it is in the United States or internationally, I am eager to contribute my passion, dedication, and drive for growth as a developer to your projects!</p>
+            </section>
+        </main>
+    )
 }
-
 export default Contact
+// import { useState } from 'react'
+// import { Button } from '@material-tailwind/react'
+
+// const Contact = () => {
+//     const [name, setName] = useState('')
+//     const [email, setEmail] = useState('')
+//     const [message, setMessage] = useState('')
+//     const [errorMessage, setErrorMessage] = useState('')
+
+//     const handleInputChange = (e) => {
+//         const { target } = e;
+//         const inputType = target.name
+//         const inputValue = target.value
+
+//         if (inputType === 'email') {
+//             setEmail(inputValue)
+//         } else if (inputType === 'name') {
+//             setName(inputValue) ``
+//         } else {
+//             setMessage(inputValue)
+//         }
+//     };
+
+//     const handleFormSubmit = (e) => {
+//         e.preventDefault();
+
+//         if (!name) {
+//             setErrorMessage('Name data required')
+//             return
+//         }
+
+//         if (!email) {
+//             setErrorMessage('Email address required')
+//             return
+//         }
+
+//         if (!message) {
+//             setErrorMessage('Message data required')
+//             return
+//         }
+
+//         // Construct mailto link
+//         const mailtoLink = `raheemsenegal@outlook.com?subject=Contact from ${encodeURIComponent(name)}&body=${encodeURIComponent(message + "\n\nReply to: " + email)}`;
+//         // Open default email client
+//         window.location.href = mailtoLink;
+
+//         // Reset the form fields
+//         setName('')
+//         setEmail('')
+//         setMessage('')
+//         setErrorMessage('') // Clear error message on successful submission
+//     };
+
+//     return (
+//         <section className='contact-background'>
+//             <h2 className='email-me-h2'>Email Me 😊</h2>
+//             <form className="form" onSubmit={handleFormSubmit}>
+//                 <input
+//                     className='contact-input' id='contact-name'
+//                     type="text"
+//                     value={name}
+//                     name='name'
+//                     onChange={handleInputChange}
+//                     placeholder='Name'
+//                 />
+//                 <input
+//                     className='contact-input'
+//                     id='email'
+//                     type="email"
+//                     value={email}
+//                     name='email'
+//                     onChange={handleInputChange}
+//                     placeholder='Email'
+//                 />
+//                 <textarea
+//                     id='textarea'
+//                     value={message}
+//                     name='message'
+//                     onChange={handleInputChange}
+//                     placeholder='Enter text here'
+//                 />
+//                 {/* <button type="submit">Submit</button> */}
+//                 <Button variant="outlined" type="submit" className="rounded-full">Submit</Button>
+//             </form>
+//             {errorMessage && (
+//                 <div>
+//                     <p className="error-text">{errorMessage}</p>
+//                 </div>
+//             )}
+//         </section>
+//     );
+// }
+
+// export default Contact
